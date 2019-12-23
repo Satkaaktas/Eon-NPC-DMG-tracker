b2730e509406d54f731f5fdf318a0ca0ebf9b159
Satkaaktas <Timmy_nitr0@hotmail.com>
2019-12-23 17:30:03 +0100

    Initial commit

---
+++ Assets/Scripts/DamageSquare.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSquare : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject spot;
+	
+	private Spot[,] spots;
+	private int columns = 10;
+	private int damage;
+	private float spriteHeight = 100;
+	private float spriteWidth = 100;
+	private float height;
+	private float width;
+
+
+	public int damageColumns
+	{
+		get { return columns; }
+		set { columns = value; }
+	}
+
+	public int damageTaken
+	{
+		get { return damage; }
+		set { damage = value; }
+	}
+
+	private void Start()
+	{
+		spots = new Spot[10, 10];
+		
+		spriteHeight *= spot.transform.localScale.y;
+		spriteWidth *= spot.transform.localScale.x;
+		height = spriteHeight * spots.GetLength(0);
+		width = spriteWidth * spots.GetLength(1);
+
+		for (int i = 0; i < spots.GetLength(0); i++)
+		{
+			for (int j = 0; j < spots.GetLength(1); j++)
+			{
+				spots[i, j] = Instantiate(spot).GetComponent<Spot>();
+				spots[i, j].transform.parent = transform;
+				//Setting position
+				spots[i, j].transform.localPosition = Vector3.up * height * 0.5f -(Vector3.up *  spriteHeight * i);
+				spots[i, j].transform.localPosition += Vector3.right * width * 0.5f - (Vector3.right * spriteWidth * j);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Spot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpotFill {Empty, Partially, Fully }
+
+public class Spot : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] images;
+	private SpotFill filled;
+
+	public SpotFill spotfill
+	{
+		get { return filled; }
+		set { filled = value; setFilled(filled); }
+	}
+
+	void setFilled(SpotFill fill)
+	{
+		switch(fill)
+		{
+			case SpotFill.Empty:
+				for (int i = 1; i < 3; i++)
+				{
+					images[i].SetActive(false);
+				}
+				break;
+			case SpotFill.Partially:
+				images[1].SetActive(true);
+				images[2].SetActive(false);
+				break;
+
+			case SpotFill.Fully:
+				images[1].SetActive(false);
+				images[2].SetActive(true);
+				break;
+		}
+	}
+}
